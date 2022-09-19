7c8912316979221b8b217aec355c7d9893c82cbe
Vagapova Valeriia <lera.vagapova@gmail.com>
2022-09-19 10:29:16 +0300

    added last callback

--- Practice_lesson1909_react22s/async_callback/callback02.js
+++ Practice_lesson1909_react22s/async_callback/callback02.js
@@ -1,15 +1,15 @@
-'use strict';
+"use strict";
 
 // Sample usage - do not modify
 const sumGrades = (grades, callback) => {
-    // simulate expensive operation
-    setTimeout(() => {
-        const sum = grades.reduce((total, current) => total + current, 0);
-        if (callback) {
-            callback(sum); // call the success callback with the sum
-        }
-    }, 1000);
-}
+  // simulate expensive operation
+  setTimeout(() => {
+    const sum = grades.reduce((total, current) => total + current, 0);
+    if (callback) {
+      callback(sum); // call the success callback with the sum
+    }
+  }, 1000);
+};
 
 /**
 Update the calculateSum function such that the console.log("The sum is: X") runs after the sumGrades function has finished executing. Also, replace X with the actual sum of the grades.
@@ -19,9 +19,8 @@ Note that the sumGrades function accepts a success callback as its second parame
  * @param {number[]} grades
  */
 const calculateSum = (grades) => {
-    sumGrades(grades);
-    console.log("The sum is: X");
-}
+  sumGrades(grades, (sum) => console.log(`The sum is ${sum}`));
+};
 
 // Sample usage - do not modify
 calculateSum([18, 10]);
