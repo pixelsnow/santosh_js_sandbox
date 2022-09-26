5563b2bf8d302b723b47a4f6c588417416588df5
Vagapova Valeriia <lera.vagapova@gmail.com>
2022-09-26 13:36:04 +0300

    added async tasks

---
+++ Practice_lessons2609_react22s/async_await/async_await07/async_await07.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/*
+Complete the getTweetDetails function such that it fetches the following endpoint
+https://programmingjs-90a13-default-rtdb.europe-west1.firebasedatabase.app/tweet.json
+and then calls the showAuthorName function with the full name of tweet author
+(the full name should contain a space character  between the first name and the last name).
+You should use async/await */
+
+const getTweetDetails = () => {
+    // TODO using async/await
+
+}
+
+// do NOT modify this function
+function showAuthorName(fullName) {
+    console.log(fullName);
+}
+
+// Sample usage - do not modify
+getTweetDetails();
