88be605769e2ded0530e6d236502b0fab6e3a43f
Vagapova Valeriia <lera.vagapova@gmail.com>
2022-09-26 16:54:09 +0300

    added the final working version of the parcel bundler

---
+++ Practice_lessons2609_react22s/throwaway/parcelBundler/dist/index.816e7b21.js
@@ -0,0 +1,3 @@
+console.log("heya");
+
+//# sourceMappingURL=index.816e7b21.js.map
---
+++ Practice_lessons2609_react22s/throwaway/parcelBundler/src/app.js
@@ -0,0 +1,6 @@
+import name from "./multipleFiles.js";
+import simple_number_formatter from "@skalwar/simple_number_formatter";
+console.log("heya");
+console.log("heya again", name);
+const result = simple_number_formatter(95563456);
+console.log(result);
