6416f558e46ac0b6540cd16c79b3291ea0f11a19
Vagapova Valeriia <lera.vagapova@gmail.com>
2022-10-01 23:09:23 +0300

    completed misc1 and misc2 tasks

--- Practice_lesson1909_react22s/fetch/misc/misc1.js
+++ Practice_lesson1909_react22s/fetch/misc/misc1.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /**
 There is an old piece of code on StackOverflow that calculates the perimeter of a square.
@@ -8,12 +8,14 @@ Then, get rid of the var declarations and feel free to remove unnecessary cons
 
 const getSquarePerimeter = (side) => {
   console.log(result);
-  var result = 0;
+  var result;
+  console.log(result);
+  result = 0;
   console.log(result);
   result = side * 4;
   console.log(result);
   return result;
-}
+};
 
 // Sample usage - do not modify
 console.log(getSquarePerimeter(5)); // 20
--- Practice_lesson1909_react22s/fetch/misc/misc2/misc2.js
+++ Practice_lesson1909_react22s/fetch/misc/misc2/misc2.js
@@ -1,23 +1,26 @@
-'use strict';
+"use strict";
 
 /**
 Every time you click on the Start game button, it is currently calling the startGame() and logAnalytics() functions.
 Update the event listener such that it only calls the startGame() once, but keeps calling logAnalytics() every time.
 **/
 
+let firstTime = true;
 const button = document.querySelector("#app-button");
 button.addEventListener("click", () => {
+  if (firstTime) {
     startGame();
-    logAnalytics();
+    firstTime = false;
+  }
+  logAnalytics();
 });
-
 
 // do not modify
 function startGame() {
-    console.log("game started!");
+  console.log("game started!");
 }
 
 // do not modify
 function logAnalytics() {
-    console.log("log analytics");
+  console.log("log analytics");
 }
