a045a55e37c1a4b768d9f84c5f954c9e6744a66f
Vagapova Valeriia <lera.vagapova@gmail.com>
2022-10-09 23:47:30 +0300

    add delete item buttons, but there's lags

--- Practice0310/todoapp/script.js
+++ Practice0310/todoapp/script.js
@@ -1,44 +1,65 @@
-// write to-do app
-
 const form = document.querySelector("#item-form");
 const itemInput = document.querySelector("#item");
 const list = document.querySelector("#list");
 const clear = document.querySelector("#clear-button");
 
+// Fetch list items from localStorage
 let itemsArray = localStorage.getItem("items")
   ? JSON.parse(localStorage.getItem("items"))
   : [];
 
-/*
-localStorage.setItem("items", JSON.stringify(itemsArray));
-const data = JSON.parse(localStorage.getItem("items"));
- */
-
-const liMaker = (text) => {
+// Adds an item to HTML
+const renderItem = (text) => {
   const li = document.createElement("li");
   li.textContent = text;
+  const btn = document.createElement("button");
+  btn.classList.add("delete-item");
+  btn.textContent = "x";
+  li.appendChild(btn);
   list.appendChild(li);
 };
 
+// Add all items to HTML
+itemsArray.forEach((item) => {
+  renderItem(item);
+});
+
+// Adds an item to the list
 const addItem = (e) => {
   e.preventDefault();
-  console.log(itemInput.value);
-  itemsArray.push(itemInput.value);
-  liMaker(itemInput.value);
+  itemsArray.push(itemInput.value); // add item to the array
+  renderItem(itemInput.value); // render item
+  // update localStorage
   localStorage.setItem("items", JSON.stringify(itemsArray));
   form.reset();
 };
 
 form.addEventListener("submit", addItem);
 
-itemsArray.forEach((item) => {
-  liMaker(item);
-});
-
+// Clears the list
 const clearList = () => {
-  localStorage.clear();
+  localStorage.clear(); // clear localStorage
+  // clear HTML
   while (list.firstChild) list.removeChild(list.firstChild);
-  itemsArray = [];
+  itemsArray = []; // clear array
 };
 
 clear.addEventListener("click", clearList);
+
+/* listItems.forEach((item) => {
+  console.log(item.firstChild);
+  item.firstChild.addEventListener("click", console.log("del"));
+}); */
+
+const deleteItem = (e) => {
+  const del = e.currentTarget.parentElement;
+  del.remove();
+};
+
+const deleteButtons = document.querySelectorAll(".delete-item");
+/* const listItems = document.querySelectorAll("#list li"); */
+
+deleteButtons.forEach((btn) => {
+  console.log(btn);
+  btn.addEventListener("click", deleteItem);
+});
