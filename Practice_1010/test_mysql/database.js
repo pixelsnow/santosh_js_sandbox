8b4ed1f1abb7b6f3d1d265e600c4a6e0469460df
Vagapova Valeriia <lera.vagapova@gmail.com>
2022-10-10 13:17:12 +0300

    add working mysql test

--- Practice_1010/test_mysql/database.js
+++ Practice_1010/test_mysql/database.js
@@ -1,16 +1,23 @@
-const mysql = require("mysql");
+// initialising mysql package manager and put into a variable
+const mysql = require("mysql2"); // mysql property from package.json (?)
+
+// boilerplate to make a connection
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
+  host: "127.0.0.1",
+  user: "testUser",
+  password: "secretword",
   database: "testdb",
 });
 
+// connect to mysql
 connection.connect();
 
+// make a query
+// error, results, fields are not keywords but variables, names can be any
 connection.query(`select * from tasks`, (error, results, fields) => {
   if (error) return console.log(error);
   return console.log(results);
 });
 
+// close connection
 connection.end();
